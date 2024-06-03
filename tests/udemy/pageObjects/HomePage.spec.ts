73c700ef43fb14f6547db7ddf81896364b679b65
sandeep <sandeepdtu360@gmail.com>
2024-06-03 11:26:31 +0530

    Modify verify home page test

--- tests/udemy/pageObjects/HomePage.spec.ts
+++ tests/udemy/pageObjects/HomePage.spec.ts
@@ -3,14 +3,22 @@ export class HomePage{
     private page:Page
     private logo: Locator;
     private category: Locator;
+    private searchBar:Locator;
+    private searchIcon:Locator;
     constructor(page: Page){
         this.page=page;
         this.logo=page.locator('[alt="Udemy"]').first();
         this.category=page.locator('.link-bar-module--categories--iqRvl li');
+        this.searchBar=page.getByPlaceholder("Search for anything");
+        this.searchIcon=page.getByLabel('Submit search');
     }
     async navigateHomePage(){
         await this.logo.isVisible();
         await this.logo.click();
+    }
+    async searchItem(searchTerm: string){
+        await this.searchBar.fill(searchTerm);
+        await this.searchIcon.click();
     }
 
 
--- tests/udemy/tests/verify-homePage.spec.ts
+++ tests/udemy/tests/verify-homePage.spec.ts
@@ -1,9 +1,15 @@
-import{test} from '@playwright/test';
+import{expect, test} from '@playwright/test';
 import { LoginPage } from '../pageObjects/LoginPage.spec';
 import { HomePage } from '../pageObjects/HomePage.spec';
 test.describe('verify home page',()=>{
     test('verify cateorgyu ',async({page})=>{
+        const searchTerm='java'
         const homePage= new HomePage(page);
         const loginPage= new LoginPage(page);
+        await loginPage.navigateHomepage();
+        await homePage.searchItem(searchTerm);
+        expect( await page.url()).toContain(searchTerm)
+        await page.waitForTimeout(10000);
+  
     })
 })
