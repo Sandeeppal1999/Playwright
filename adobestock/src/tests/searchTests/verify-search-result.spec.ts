a6992154828f307e5e04e50cb19f60eb7dd4e651
sandeep <sandeepdtu360@gmail.com>
2024-07-22 02:50:57 +0530

    minor change

--- adobestock/src/tests/searchTests/verify-search-result.spec.ts
+++ adobestock/src/tests/searchTests/verify-search-result.spec.ts
@@ -4,7 +4,6 @@ test.describe('Given I am @anonymous adobe user',()=>{
     test('Verify search result ',async({page})=>{
         const commonHome = new CommonHome(page);
         await page.goto('/');
-        commonHome.verifyURL('https://stock.adobe.com/in');
         const searchTerm= 'dog'
         await page.locator('[name="keyword"]').fill(searchTerm);
         await page.locator('[data-t="search-icon"]').click();
